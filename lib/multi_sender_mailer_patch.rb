63a3fc2214ca8e9556ff9091dbc9e61651cbff08
Vignesh EsakkiMuthu <vemcse@gmail.com>
2023-02-25 00:02:23 +0530

    Print the log

--- lib/multi_sender_mailer_patch.rb
+++ lib/multi_sender_mailer_patch.rb
@@ -14,27 +14,29 @@ module MultiSenderMailerPatch
 
       container = @issue || @news || @wiki_content || @message || @document || @attachments.try(:first)
       project = container.try(:project)
-      Rails.logger.info "Container #{container.class} - #{container.id} project #{project.id} - #{project.name} "
-      sender_name = Setting.plugin_redmine_multi_sender["custom_field_name"]
-      email_sender = "default"
-      sender_custom_field = CustomField.where(type: "ProjectCustomField", name: sender_name).first
-      if project.present? && sender_custom_field.present? && project.custom_field_value(sender_custom_field).present?
-         email_sender = project.custom_field_value(sender_custom_field)
-      end
-      user_name = Setting.plugin_redmine_multi_sender["#{email_sender}_from_email"]
-      token =  EmailSender.fetch_access_token!(email_sender, 'email')
-      domain = Setting.plugin_redmine_multi_sender["#{email_sender}_domain"]
-      host = Setting.plugin_redmine_multi_sender["#{email_sender}_host"]
-      port = Setting.plugin_redmine_multi_sender["#{email_sender}_port"]
-      if token.present?
-        delivery_options = { user_name: user_name,
-                         password: token,
-                         domain: domain,
-                         address: host,
-                         authentication: :xoauth2,
-                         port: port }
-        Setting.mail_from = user_name
-        self.smtp_settings.merge!(delivery_options)
+      if project.present?
+        puts "Container #{container.class} - #{container.id} project #{project.id} - #{project.name} "
+        sender_name = Setting.plugin_redmine_multi_sender["custom_field_name"]
+        email_sender = "default"
+        sender_custom_field = CustomField.where(type: "ProjectCustomField", name: sender_name).first
+        if project.present? && sender_custom_field.present? && project.custom_field_value(sender_custom_field).present?
+           email_sender = project.custom_field_value(sender_custom_field)
+        end
+        user_name = Setting.plugin_redmine_multi_sender["#{email_sender}_from_email"]
+        token =  EmailSender.fetch_access_token!(email_sender, 'email')
+        domain = Setting.plugin_redmine_multi_sender["#{email_sender}_domain"]
+        host = Setting.plugin_redmine_multi_sender["#{email_sender}_host"]
+        port = Setting.plugin_redmine_multi_sender["#{email_sender}_port"]
+        if token.present?
+          delivery_options = { user_name: user_name,
+                           password: token,
+                           domain: domain,
+                           address: host,
+                           authentication: :xoauth2,
+                           port: port }
+          Setting.mail_from = user_name
+          self.smtp_settings.merge!(delivery_options)
+        end
       end
       mail_without_helpdesk(headers, &block)
     end      
