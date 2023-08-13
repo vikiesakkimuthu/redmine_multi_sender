2d0f4e166f38260ca57ceeba530aa33eff4aceac
Vignesh <vemcse@gmail.com>
2023-08-13 23:35:12 +0530

    Fix forgot password and send email for user email and from email take from project

--- lib/multi_sender_mailer_patch.rb
+++ lib/multi_sender_mailer_patch.rb
@@ -11,23 +11,29 @@ module MultiSenderMailerPatch
   module InstanceMethods
 
     def mail_with_helpdesk(headers={}, &block)
-
       container = @issue || @news || @wiki_content || @message || @document || @attachments.try(:first)
       project = container.try(:project)
-      Rails.logger.info "Container #{container.class} - #{container.id} project #{project.id} - #{project.name} "
+      # Rails.logger.info "Container #{container.class} - #{container.id} project #{project.id} - #{project.name} "
       sender_name = Setting.plugin_redmine_multi_sender["custom_field_name"]
       email_sender = "default"
       sender_custom_field = CustomField.where(type: "ProjectCustomField", name: sender_name).first
       if project.present? && sender_custom_field.present? && project.custom_field_value(sender_custom_field).present?
          email_sender = project.custom_field_value(sender_custom_field)
       end
-      Rails.logger.info "email_sender_name #{email_sender} Container #{container.class} - #{container.id} project #{project.id} - #{project.name} "
+      # Rails.logger.info "email_sender_name #{email_sender} Container #{container.class} - #{container.id} project #{project.id} - #{project.name} "
       user_name = Setting.plugin_redmine_multi_sender["#{email_sender}_from_email"]
       smtp_api_access_token =  EmailSender.fetch_access_token!(email_sender, 'email')
       domain = Setting.plugin_redmine_multi_sender["#{email_sender}_domain"]
       host = Setting.plugin_redmine_multi_sender["#{email_sender}_host"]
       port = Setting.plugin_redmine_multi_sender["#{email_sender}_port"]
-      from_email = Setting.plugin_redmine_multi_sender["#{email_sender}_sender_email"]
+      from_email = user_name
+      if project.present?
+        sender_email_field = ProjectCustomField.find_by_name "helpdesk-sender-email"
+        if sender_email_field.try(:id).present?
+          sender_email = project.custom_field_value(sender_email_field.try(:id))
+          from_email = sender_email if sender_email.present?
+        end
+      end
       # if access_token.present?
       delivery_options = { user_name: user_name,
                        password: smtp_api_access_token,
@@ -35,7 +41,7 @@ module MultiSenderMailerPatch
                        address: host,
                        authentication: :xoauth2,
                        port: port }
-      Setting.mail_from = from_email.present? ? from_email : user_name
+      Setting.mail_from = from_email
       self.smtp_settings.merge!(delivery_options)
       # end
       mail_without_helpdesk(headers, &block)
